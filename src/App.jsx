a7c3845e23f9f5d24b1c2819f5e19064c249b442
Charles <78848071+hacker-kamiri@users.noreply.github.com>
2022-01-29 21:30:45 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import './App.js';
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <canvas className="canvas" id="canvas"></canvas>
+        )
+    }
+}
